cf5fd29bf957b7c8d797577de5e84a656ec00a7c
serbulevhenii <56Serbul80>
2020-09-14 15:02:51 +0300

    v0.4 Добавлено меню с выбором времени

--- bot_handlers.py
+++ bot_handlers.py
@@ -1,17 +1,19 @@
-from bot import bot       # Импортируем объект бота
-from messages import *    # Инмпортируем все с файла сообщений
-from db import *          # Импортируем все методы из файла для базы данных
-import weather_api        # Импортируем все методы из файла для погоды
-import telebot.types      # Импортируем типы телеграма API
-import markups            # Импортируем кнопки для бота
-import emoji              # Импортируем смайлы http://www.unicode.org/emoji/charts/full-emoji-list.html
+from bot import bot  # Импортируем объект бота
+from messages import *  # Инмпортируем все с файла сообщений
+from db import *  # Импортируем все методы из файла для базы данных
+import weather_api  # Импортируем все методы из файла для погоды
+import telebot.types  # Импортируем типы телеграма API
+import markups  # Импортируем кнопки для бота
+import inlineKeyboard  # Импортируем инлайн кавиатуры
+import emoji  # Импортируем смайлы http://www.unicode.org/emoji/charts/full-emoji-list.html
 
 
 @bot.message_handler(commands=['start'])
 # Выполняется, когда пользователь вызывает /start
 def send_welcome(message):
     if check_in_db(column='user_id', data_check=message.chat.id):
-        bot.send_message(message.chat.id, f'{HELLO_AGAIN_MESSAGE}, {message.chat.first_name}!', reply_markup=markups.markup_main)
+        bot.send_message(message.chat.id, f'{HELLO_AGAIN_MESSAGE}, {message.chat.first_name}!',
+                         reply_markup=markups.markup_main)
     else:
         add_user_in_db(user_name=message.chat.username, user_id=message.chat.id)
         bot.send_message(message.chat.id,
@@ -36,11 +38,57 @@ def unsubscribe(message):
     unsubscribe_db(user_id=message.chat.id)
     bot.send_message(message.chat.id, UNSUBSCRIBE)
 
-@bot.message_handler(commands=['time'])
+
+# @bot.message_handler(commands=['time'])
+# # Выполняется, когда пользователь вызывает /time
+# def set_time_notify_in_db(message):
+#     set_time_notify(user_id=message.chat.id, time='07:00')
+#     bot.send_message(message.chat.id, SET_TIME_NOTIFY)
+
+
 # Выполняется, когда пользователь вызывает /time
-def set_time_notify_in_db(message):
-    set_time_notify(user_id=message.chat.id, time='07:00')
-    bot.send_message(message.chat.id, SET_TIME_NOTIFY)
+def set_time_notify_in_db(callback_query, time):
+    time_notify = time
+    set_time_notify(user_id=callback_query.from_user.id, time=time_notify)
+
+
+# Отображение 2х кнопок "Настройки бота": настройка времени и настройка местоположения
+def start_menu_settings(message):
+    bot.send_message(message.chat.id, 'Настройки бота:', reply_markup=inlineKeyboard.inline_kb_settings)
+
+
+# Отображения времени выбора
+def start_menu_all_times(callback_query):
+    bot.send_message(callback_query.from_user.id, 'Выберите подходящее для Вас время, для ведомлений.',
+                     reply_markup=inlineKeyboard.inline_kb_all_times)
+
+# Реакция нажатия на кнопки из меню: "Настройки бота"
+@bot.callback_query_handler(func=lambda c: c.data and c.data.startswith('menu'))
+def set_time_notify_menu(callback_query: telebot.types.CallbackQuery):
+    if callback_query.data == 'menu_btn_geo':
+        bot.answer_callback_query(callback_query.id)
+        bot.send_message(callback_query.from_user.id, 'Передача местоположения в разработке')
+    elif callback_query.data == 'menu_btn_notify':
+        bot.answer_callback_query(callback_query.id)
+        start_menu_all_times(callback_query)
+
+
+# Реакция кнопок со временем уведомлений
+@bot.callback_query_handler(func=lambda c: c.data and c.data.startswith('btn'))
+def set_time_notify_menu(callback_query: telebot.types.CallbackQuery):
+    if callback_query.data == 'btn05:00':
+        bot.answer_callback_query(callback_query.id)
+        set_time_notify(user_id=callback_query.from_user.id, time=callback_query.data[0])
+        bot.send_message(callback_query.from_user.id, 'Время уведомления установлено на 05:00')
+    elif callback_query.data == 'btn06:00':
+        bot.answer_callback_query(callback_query.id)
+        bot.send_message(callback_query.from_user.id, 'Время уведомления установлено на 06:00')
+
+
+# @bot.callback_query_handler(func=lambda c: c.data == '07:00')
+# def process_callback_btn(callback_query: telebot.types.CallbackQuery):
+#     bot.answer_callback_query(callback_query.id)
+#     bot.send_message(callback_query.from_user.id, 'Нажата 07:00')
 
 
 # @bot.message_handler(content_types=["text"])  # Любой текст
@@ -64,7 +112,7 @@ def handle_text(message):
     elif 'помощь' in text:
         bot.send_message(message.chat.id, 'Пока в разработке...')
     elif 'настройка бота' in text:
-        bot.send_message(message.chat.id, 'Пока в разработке...')
+        start_menu_settings(message)
     elif 'подписаться' in text:
         # проверить если пользователь в базе данных, если нет, то добавить
         if check_in_db(column='user_id', data_check=message.chat.id):
---
+++ inlineKeyboard.py
@@ -0,0 +1,35 @@
+import telebot
+
+inline_btn_menu_geo = telebot.types.InlineKeyboardButton('Настройки местоположения', callback_data='menu_btn_geo')
+inline_btn_menu_notify = telebot.types.InlineKeyboardButton('Выбор времени уведомлений', callback_data='menu_btn_notify')
+inline_kb_settings = telebot.types.InlineKeyboardMarkup(row_width=1).add(inline_btn_menu_geo,
+                                                              inline_btn_menu_notify)
+
+
+
+inline_btn_1 = telebot.types.InlineKeyboardButton('05:00', callback_data='btn05:00')
+inline_btn_2 = telebot.types.InlineKeyboardButton('06:00', callback_data='btn06:00')
+inline_btn_3 = telebot.types.InlineKeyboardButton('07:00', callback_data='btn07:00')
+inline_btn_4 = telebot.types.InlineKeyboardButton('08:00', callback_data='btn08:00')
+inline_btn_5 = telebot.types.InlineKeyboardButton('09:00', callback_data='btn09:00')
+inline_btn_6 = telebot.types.InlineKeyboardButton('10:00', callback_data='btn10:00')
+inline_btn_7 = telebot.types.InlineKeyboardButton('11:00', callback_data='btn11:00')
+inline_btn_8 = telebot.types.InlineKeyboardButton('12:00', callback_data='btn12:00')
+inline_btn_9 = telebot.types.InlineKeyboardButton('13:00', callback_data='btn13:00')
+inline_btn_10 = telebot.types.InlineKeyboardButton('14:00', callback_data='btn14:00')
+inline_btn_11 = telebot.types.InlineKeyboardButton('15:00', callback_data='btn15:00')
+inline_btn_12 = telebot.types.InlineKeyboardButton('16:00', callback_data='btn16:00')
+inline_btn_13 = telebot.types.InlineKeyboardButton('17:00', callback_data='btn17:00')
+inline_btn_14 = telebot.types.InlineKeyboardButton('18:00', callback_data='btn18:00')
+inline_btn_15 = telebot.types.InlineKeyboardButton('19:00', callback_data='btn19:00')
+inline_btn_16 = telebot.types.InlineKeyboardButton('20:00', callback_data='btn20:00')
+inline_btn_17 = telebot.types.InlineKeyboardButton('21:00', callback_data='btn21:00')
+inline_btn_18 = telebot.types.InlineKeyboardButton('22:00', callback_data='btn22:00')
+inline_btn_19 = telebot.types.InlineKeyboardButton('23:00', callback_data='btn23:00')
+
+inline_kb_all_times = telebot.types.InlineKeyboardMarkup().add(inline_btn_1, inline_btn_2, inline_btn_3, inline_btn_4,
+                                                               inline_btn_5, inline_btn_6, inline_btn_7, inline_btn_8,
+                                                               inline_btn_9, inline_btn_10, inline_btn_11, inline_btn_12,
+                                                               inline_btn_13, inline_btn_14, inline_btn_15, inline_btn_16,
+                                                               inline_btn_17, inline_btn_18, inline_btn_19)
+
