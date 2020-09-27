47a2d6699132c2b5d1a936798656373abb3ea4e5
SerbulEvhenii <serbulwork@gmail.com>
2020-09-27 13:15:30 +0300

    v0.7.6

--- bot_handlers.py
+++ bot_handlers.py
@@ -223,6 +223,5 @@ if __name__ == "__main__":
     init_db()
     print('База данных инициализированна...')
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    print('Сервер с ботом запущен...')
 
 
--- test_scheduler.py
+++ test_scheduler.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from bot_handlers import notify_weather
+from .bot_handlers import notify_weather
 
 sched = BlockingScheduler()
 
@@ -7,8 +7,9 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', seconds=30)
 def timed_job():
     notify_weather()
-    print('Тестирование расписания, я вызываюсь кажыде 30 секунд.')
+    print('Тестирование расписания, я вызываюсь каждые 30 секунд.')
 
 
 if __name__ == '__main__':
+    print('Расписание запущено...')
     sched.start()
