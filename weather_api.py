ef76205d1a32f02fe948124ecc0d1caddacd1546
SerbulEvhenii <serbulwork@gmail.com>
2020-10-05 23:11:54 +0300

    v0.8 (Большое обновление, почти полный функционал)

--- weather_api.py
+++ weather_api.py
@@ -46,20 +46,29 @@ def show_current_weather(user_id):
 
 
 def show_current_daily_weather(user_id):
-    temp_min = round((get_weather(user_id))['daily'][0]['temp']['min'], 0)  # [0] - сегодня, 1 - завтра
-    temp_max = round((get_weather(user_id))['daily'][0]['temp']['max'], 0)  # [0] - сегодня, 1 - завтра
-    pop = int((get_weather(user_id))['daily'][0]['pop'] * 100)  # Вероятность осадков % (Precipitation)
-    return emoji.emojize(f'Погода на сегодня в Киеве:\n'
-                         f'• :sun_behind_cloud: температура воздуха:\n'
-                         f'мин. +{temp_min} макс. +{temp_max}\n'
-                         f'• :cloud_with_rain: вероятность осадков: {pop}%')
+    city = (get_weather(user_id))['timezone']
+    temp_min = round((read_json())['daily'][0]['temp']['min'], 0)  # [0] - сегодня, 1 - завтра
+    temp_max = round((read_json())['daily'][0]['temp']['max'], 0)  # [0] - сегодня, 1 - завтра
+    feel_temp_morn = round((read_json())['daily'][0]['feels_like']['morn'], 0)
+    feel_temp_day = round((read_json())['daily'][0]['feels_like']['day'], 0)
+    feel_temp_eve = round((read_json())['daily'][0]['feels_like']['eve'], 0)
+    pop = int((read_json())['daily'][0]['pop'] * 100)  # Вероятность осадков % (Precipitation)
+    return emoji.emojize(f'Погода на сегодня в {city}:\n'
+                         f':sun_behind_cloud: температура воздуха:\n'
+                         f'• мин. +{temp_min} макс. +{temp_max}\n'
+                         f'Ощущается:\n'
+                         f'• утром +{feel_temp_morn}\n'
+                         f'• в обед +{feel_temp_day}\n'
+                         f'• вечером +{feel_temp_eve}\n'
+                         f':cloud_with_rain: вероятность осадков: {pop}%')
 
 
 def show_tomorrow_weather(user_id):
-    temp_min = round((get_weather(user_id))['daily'][1]['temp']['min'], 0)  # [1] - сегодня, 2 - завтра
-    temp_max = round((get_weather(user_id))['daily'][1]['temp']['max'], 0)  # [1] - сегодня, 2 - завтра
-    pop = int((get_weather(user_id))['daily'][1]['pop'] * 100)  # Вероятность осадков % (Precipitation)
-    return emoji.emojize(f'Погода на завтра в Киеве:\n'
+    city = (get_weather(user_id))['timezone']
+    temp_min = round((read_json())['daily'][1]['temp']['min'], 0)  # [1] - сегодня, 2 - завтра
+    temp_max = round((read_json())['daily'][1]['temp']['max'], 0)  # [1] - сегодня, 2 - завтра
+    pop = int((read_json())['daily'][1]['pop'] * 100)  # Вероятность осадков % (Precipitation)
+    return emoji.emojize(f'Погода на сегодня в {city}:\n'
                          f'• :sun_behind_cloud: температура воздуха:\n'
                          f'мин. +{temp_min} макс. +{temp_max}\n'
                          f'• :cloud_with_rain: вероятность осадков: {pop}%')
