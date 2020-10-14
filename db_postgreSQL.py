b06868487d407c872d47cc12002a4759d72fcff0
SerbulEvhenii <serbulwork@gmail.com>
2020-10-14 23:37:15 +0300

    v0.9 (Обновление на PostgreSQL)

--- db_postgreSQL.py
+++ db_postgreSQL.py
@@ -123,9 +123,9 @@ def set_geoposition(conn, user_id: int, latit: float, long: float):
 @ensure_connection
 def get_geoposition(conn, user_id: int):
     c = conn.cursor()
-    c.execute('SELECT latitude FROM bot_users WHERE user_id=%s;', (user_id))
+    c.execute('SELECT latitude FROM bot_users WHERE user_id=%s;', [user_id])
     latitude = c.fetchone()[0]
-    c.execute(f'SELECT longitude FROM bot_users WHERE user_id=%s;', (user_id))
+    c.execute(f'SELECT longitude FROM bot_users WHERE user_id=%s;', [user_id])
     longitude = c.fetchone()[0]
     return latitude, longitude
 
