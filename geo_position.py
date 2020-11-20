5b376d53fa7695fb8b96e6cb031f5ce3c98e64b9
SerbulEvhenii <serbulwork@gmail.com>
2020-11-20 12:44:06 +0200

    v0.9.9 (обработка недоступности API)

--- geo_position.py
+++ geo_position.py
@@ -2,16 +2,10 @@
 from geopy.geocoders import Nominatim
 from db_postgreSQL import set_city_user_db
 
-# def set_city_geopy(user_id, latit, long):
-#     geolocator = Nominatim(user_agent="SEA")
-#     location = geolocator.reverse(f"{latit}, {long}", language='ru')
-#     loc_dict = location.raw
-#     city = loc_dict['address']['city'] + ', ' + loc_dict['address']['country']
-#     set_city_user_db(user_id=user_id, geopy_city=city)
 
 def set_city_geopy(user_id, latit, long):
     geolocator = Nominatim(user_agent="SEA")
-    location = False
+    location = geolocator.reverse(f"{latit}, {long}", language='ru')
     if location:
         loc_dict = location.raw
         city = loc_dict['address']['city'] + ', ' + loc_dict['address']['country']
